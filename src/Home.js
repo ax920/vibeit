6997bf1f9365e347006d3a52e7d818b951d9f66b
Alvin Xu <alvin.xu920@gmail.com>
2020-07-04 13:50:08 -0600

    create chart displaying stats

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useHistory } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import {
+    ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip
+} from 'recharts';
 const axios = require('axios').default;
 const querystring = require('query-string');
 
@@ -21,21 +24,84 @@ export default class Home extends React.Component {
         }
     }
     async componentDidMount() {
-        const get_top_tracks_url = 'https://api.spotify.com/v1/me/top/tracks';
+        const openRequest = window.indexedDB.open('toDoList');
         const get_saved_tracks_url = 'https://api.spotify.com/v1/me/tracks';
-        if (localStorage.getItem('all songs') === null) {
-            this.getAllSavedTracks(get_saved_tracks_url, 50, 0);
-        } else {
-            this.state.allSongs = JSON.parse(localStorage.getItem('all songs') || "[]");
-        }
-        this.getTopTracks(get_top_tracks_url); // TODO set it to localstorage as well so don't have to do everytime
-        console.log(this.state.allSongs)
-        if (localStorage.getItem('feature 1') === null) {
-            await this.mapFeaturesToSavedTracks(0);
-        } else {
-            console.log(JSON.parse(localStorage.getItem('all songs') || "[]")); // TODO parse into state
-        }
-        console.log(this.state.audioTrackFeatures)
+        // if (localStorage.getItem('all songs') === null) {
+        console.log('access token', this.state.jwt.access_token)
+        this.getAllSavedTracks(get_saved_tracks_url, 50, 0);
+        // } else {
+        // this.state.allSongs = JSON.parse(localStorage.getItem('all songs') || "[]");
+        // }
+        this.getTopTracks(); // TODO set it to localstorage as well so don't have to do everytime
+        // await this.mapFeaturesToSavedTracks(0);
+        // if (localStorage.getItem('feature 1') === null) {
+        // } else {
+        //     console.log(JSON.parse(localStorage.getItem('all songs') || "[]")); // TODO parse into state
+        // }
+    }
+
+    getTopTracks() {
+        const get_top_tracks_url = 'https://api.spotify.com/v1/me/top/tracks';
+        axios.get(get_top_tracks_url, {
+            headers: {
+                'Authorization': 'Bearer ' + this.state.jwt.access_token
+            },
+            params: {
+                limit: 50,
+                offset: 0
+            }
+        }).then(response => {
+            // console.log("FIRST", response.data.items);
+            const songs = response.data.items;
+            songs.map(song => {
+                this.setState(prevState => ({
+                    jwt: prevState.jwt,
+                    topSongs: [...prevState.topSongs, song]
+                }))
+            });
+            axios.get(get_top_tracks_url, {
+                headers: {
+                    'Authorization': 'Bearer ' + this.state.jwt.access_token
+                },
+                params: {
+                    limit: 100,
+                    offset: 50
+                }
+            }).then(response => {
+                // console.log("SECOND", response.data.items);
+                const songs = response.data.items;
+                songs.map((song, index) => {
+                    if (index !== 0) {
+                        this.setState(prevState => ({
+                            jwt: prevState.jwt,
+                            topSongs: [...prevState.topSongs, song]
+                        }))
+                    }
+                });
+                axios.get(get_top_tracks_url, {
+                    headers: {
+                        'Authorization': 'Bearer ' + this.state.jwt.access_token
+                    },
+                    params: {
+                        limit: 150,
+                        offset: 100
+                    }
+                }).then(response => {
+                    // console.log("THIRD", response.data.items);
+                    const songs = response.data.items;
+                    songs.map((song, index) => {
+                        if (index !== 0) {
+                            this.setState(prevState => ({
+                                jwt: prevState.jwt,
+                                topSongs: [...prevState.topSongs, song]
+                            }))
+                        }
+                    });
+                    console.log("top tracks", this.state.topSongs);
+                    this.mapFeaturesToTopTracks(0);
+                });
+            });
+        });
     }
 
     mapFeaturesToSavedTracks(offset) {
@@ -58,7 +124,6 @@ export default class Home extends React.Component {
             }
         }).then(response => {
             const features = response.data.audio_features;
-            console.log(features);
             features.map(feature => {
                 this.setState(prevState => ({
                     jwt: prevState.jwt,
@@ -68,7 +133,6 @@ export default class Home extends React.Component {
                 }));
             });
             if (features.length == 0 || response.data.audio_features.length == 1) {
-                console.log("stop222");
                 let tempCopyFeatures = [...this.state.audioTrackFeatures];
                 tempCopyFeatures = tempCopyFeatures.slice(0, -1);
                 this.setState(prevState => ({
@@ -78,11 +142,10 @@ export default class Home extends React.Component {
                     audioTrackFeatures: tempCopyFeatures
                 }));
                 console.log(this.state.audioTrackFeatures)
-                // TO UNCOMMENT
-                // const indices = Math.ceil(this.state.audioTrackFeatures.length / 100);
-                // let currentAllFeatures = this.state.audioTrackFeatures;
+                const indices = Math.ceil(this.state.audioTrackFeatures.length / 50);
+                let currentAllFeatures = this.state.audioTrackFeatures;
                 // for (let i = 0; i < indices; i++) {
-                //     const division = currentAllFeatures.slice(0, (i + 1) * 100);
+                //     const division = currentAllFeatures.slice(0, (i + 1) * 50);
                 //     localStorage.setItem("features " + i, JSON.stringify(division));
                 // }
                 return;
@@ -90,6 +153,70 @@ export default class Home extends React.Component {
                 this.mapFeaturesToSavedTracks(offset + 100);
             }
         });
+    }
+
+    mapFeaturesToTopTracks(offset) {
+        const get_features_several_url = 'https://api.spotify.com/v1/audio-features';
+        let ids = [];
+        // for (let i = 0; i < Math.ceil(this.state.allSongs.length / 100); i++) {
+        // const index = i * 100;
+        const currSongs = this.state.topSongs.slice(offset, offset + 100);
+        currSongs.map(song => {
+            ids.push(song.id);
+        });
+        // }
+        ids = ids.join(",")
+        axios.get(get_features_several_url, {
+            headers: {
+                'Authorization': 'Bearer ' + this.state.jwt.access_token
+            },
+            params: {
+                ids: ids
+            }
+        }).then(response => {
+            const features = response.data.audio_features;
+            features.map(feature => {
+                this.setState(prevState => ({
+                    jwt: prevState.jwt,
+                    topSongs: [...prevState.topSongs],
+                    allSongs: [...prevState.allSongs],
+                    audioTrackFeatures: [...prevState.audioTrackFeatures, feature]
+                }));
+            });
+            if (features.length == 0 || response.data.audio_features.length == 1) {
+                let tempCopyFeatures = [...this.state.audioTrackFeatures];
+                tempCopyFeatures = tempCopyFeatures.slice(0, -1);
+                this.setState(prevState => ({
+                    jwt: prevState.jwt,
+                    topSongs: [...prevState.topSongs],
+                    allSongs: [...prevState.allSongs],
+                    audioTrackFeatures: tempCopyFeatures
+                }));
+                console.log("toptracks features", this.state.audioTrackFeatures.map(track => track.danceability));
+                const indices = Math.ceil(this.state.audioTrackFeatures.length / 50);
+                let currentAllFeatures = this.state.audioTrackFeatures;
+                // for (let i = 0; i < indices; i++) {
+                //     const division = currentAllFeatures.slice(0, (i + 1) * 50);
+                //     localStorage.setItem("features " + i, JSON.stringify(division));
+                // }
+
+                this.state.topSongs.map((song, i) => {
+                    console.log(song, i)
+                    const matchingFeature = this.state.audioTrackFeatures.find((feature, j) => {
+                        return feature.id === song.id;
+                    });
+                    song.features = matchingFeature;
+                })
+                this.getUpbeat();
+                return;
+            } else {
+                this.mapFeaturesToTopTracks(offset + 100);
+            }
+        });
+    }
+
+    getUpbeat() {
+
     }
 
     getAllSavedTracks(saved_tracks_url, limit, offset) {
@@ -110,7 +237,8 @@ export default class Home extends React.Component {
                     allSongs: [...prevState.allSongs, song]
                 }))
             })
-            console.log("tracksok", this.state.allSongs);
+
+            console.log("all songs - ", this.state.allSongs);
             if (songs.length == 0 || songs == null) {
                 console.log("stopppp  ")
                 // const indices = Math.ceil(this.state.allSongs.length / 250);
@@ -121,76 +249,54 @@ export default class Home extends React.Component {
                 //     localStorage.setItem("division " + i, JSON.stringify(division));
                 // }
                 console.log(this.state.allSongs);
-                localStorage.setItem('all songs', JSON.stringify(this.state.allSongs));
+                // localStorage.setItem('all songs', JSON.stringify(this.state.allSongs));
+
                 return;
             } else {
-                this.getAllSavedTracks(saved_tracks_url, limit, offset + 50);
+                // COMMENTED OUT FOR NOW TO TEST 50 tracks
+                // this.getAllSavedTracks(saved_tracks_url, limit, offset + 50);
             }
         })
     }
 
-    getTopTracks(top_tracks_url) {
-        axios.get(top_tracks_url, {
-            headers: {
-                'Authorization': 'Bearer ' + this.state.jwt.access_token
-            },
-            params: {
-                limit: 50,
-                offset: 0
-            }
-        }).then(response => {
-            // console.log("FIRST", response.data.items);
-            const songs = response.data.items;
-            songs.map(song => {
-                this.setState(prevState => ({
-                    jwt: prevState.jwt,
-                    topSongs: [...prevState.topSongs, song]
-                }))
-            });
-            axios.get(top_tracks_url, {
-                headers: {
-                    'Authorization': 'Bearer ' + this.state.jwt.access_token
-                },
-                params: {
-                    limit: 100,
-                    offset: 50
-                }
-            }).then(response => {
-                // console.log("SECOND", response.data.items);
-                const songs = response.data.items;
-                songs.map((song, index) => {
-                    if (index !== 0) {
-                        this.setState(prevState => ({
-                            jwt: prevState.jwt,
-                            topSongs: [...prevState.topSongs, song]
-                        }))
-                    }
-                });
-                axios.get(top_tracks_url, {
-                    headers: {
-                        'Authorization': 'Bearer ' + this.state.jwt.access_token
-                    },
-                    params: {
-                        limit: 150,
-                        offset: 100
-                    }
-                }).then(response => {
-                    // console.log("THIRD", response.data.items);
-                    const songs = response.data.items;
-                    songs.map((song, index) => {
-                        if (index !== 0) {
-                            this.setState(prevState => ({
-                                jwt: prevState.jwt,
-                                topSongs: [...prevState.topSongs, song]
-                            }))
-                        }
-                    });
-                });
-            });
-        });
-    }
+    // getPlaylistSongs() {
+    //     axios.get(saved_tracks_url, {
+    //         headers: {
+    //             'Authorization': 'Bearer ' + this.state.jwt.access_token
+    //         },
+    //         params: {
+    //             limit: limit,
+    //             offset: offset
+    //         }
+    //     }).then(response => {
+
+    //     });
+    // }
 
     render() {
-        return <h1>this is home</h1>
+        // console.log("test", this.state.audioTrackFeatures)
+        if (!this.state.audioTrackFeatures || this.state.audioTrackFeatures.length <= 0) {
+            return (<h1>Loading...</h1>)
+        }
+        const features = this.state.audioTrackFeatures;
+        const array = [];
+        for (let i = 0; i < features.length; i++) {
+            if (features[i]) {
+                array.push({ x: i, y: features[i].danceability });
+            }
+        }
+        console.log("array", array);
+        return (
+            <React.Fragment>
+                <h1>this is home</h1>
+                <ScatterChart width={400} height={400} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+                    <CartesianGrid />
+                    <XAxis dataKey={'x'} type="number" name='stature' unit='cm' />
+                    <YAxis dataKey={'y'} type="number" name='weight' unit='kg' />
+                    <Scatter name='A school' data={array} fill='#8884d8' />
+                    <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+                </ScatterChart>
+            </React.Fragment>
+        )
     }
 }
